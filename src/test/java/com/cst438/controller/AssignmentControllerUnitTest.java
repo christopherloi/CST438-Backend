348e68cd048ba86cb6ee09be7aab86477dae176f
JerrettR <jerrettmrosario@gmail.com>
2024-07-11 08:42:49 -0700

    Completed unit tests #1 and #2. Modified AssignmentController.java to check if an assignment's due date is within the start and end dates of the course.

---
+++ src/test/java/com/cst438/controller/AssignmentControllerUnitTest.java
@@ -0,0 +1,142 @@
+package com.cst438.controller;
+
+import com.cst438.domain.Assignment;
+import com.cst438.domain.AssignmentRepository;
+import com.cst438.domain.Section;
+import com.cst438.dto.AssignmentDTO;
+import com.cst438.dto.SectionDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * example of unit test to add a section to an existing course
+ */
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AssignmentControllerUnitTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    @Test
+    public void addAssignment() throws Exception {
+
+        MockHttpServletResponse response;
+
+        // create DTO with data for new section.
+        // the primary key, secNo, is set to 0. it will be
+        // set by the database when the section is inserted.
+        AssignmentDTO assignment = new AssignmentDTO(
+                0,
+                "db homework 3",
+                "2024-05-16",
+                "cst438",
+                1,
+                10
+        );
+
+        // issue a http POST request to SpringTestServer
+        // specify MediaType for request and response data
+        // convert section to String data and set as request content
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/assignments")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(assignment)))
+                .andReturn()
+                .getResponse();
+
+        // check the response code for 200 meaning OK
+        assertEquals(200, response.getStatus());
+
+        // return data converted from String to DTO
+        AssignmentDTO result = fromJsonString(response.getContentAsString(), AssignmentDTO.class);
+
+        // primary key should have a non zero value from the database
+        assertNotEquals(0, result.secNo());
+        // check other fields of the DTO for expected values
+        assertEquals("cst438", result.courseId());
+
+        // check the database
+        Assignment a = assignmentRepository.findById(result.id()).orElse(null);
+        assertNotNull(a);
+        assertEquals("cst438", a.getSection().getCourse().getCourseId());
+
+        // clean up after test. issue http DELETE request for section
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .delete("/assignments/"+result.id()))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(200, response.getStatus());
+
+        // check database for delete
+        a = assignmentRepository.findById(result.id()).orElse(null);
+        assertNull(a);  // section should not be found after delete
+    }
+
+    @Test
+    public void addAssignmentFailsPastEndDate() throws Exception {
+        MockHttpServletResponse response;
+
+        AssignmentDTO assignment = new AssignmentDTO(
+                0,
+                "db homework 3",
+                "2026-10-31",
+                "cst438",
+                1,
+                10
+        );
+
+        // issue a http POST request to SpringTestServer
+        // specify MediaType for request and response data
+        // convert section to String data and set as request content
+        response = mvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/assignments")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(assignment)))
+                .andReturn()
+                .getResponse();
+
+        // response should be 400, BAD_REQUEST
+        assertEquals(400, response.getStatus());
+
+        // check the expected error message
+        String message = response.getErrorMessage();
+        System.out.println(message);
+//        assertEquals("course not found cst599", message);
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> T  fromJsonString(String str, Class<T> valueType ) {
+        try {
+            return new ObjectMapper().readValue(str, valueType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
