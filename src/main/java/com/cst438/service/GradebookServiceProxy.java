6f420f18278cf625c2081340e7d5f3404831ad3a
JerrettR <jerrettmrosario@gmail.com>
2024-07-23 13:25:46 -0700

    Added to GradebookServiceProxy.java

--- src/main/java/com/cst438/service/GradebookServiceProxy.java
+++ src/main/java/com/cst438/service/GradebookServiceProxy.java
@@ -1,5 +1,9 @@
 package com.cst438.service;
 
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.dto.CourseDTO;
+import com.cst438.dto.EnrollmentDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class GradebookServiceProxy {
 
     Queue gradebookServiceQueue = new Queue("gradebook_service", true);
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
 
     @Bean
     public Queue createQueue() {
@@ -21,14 +27,44 @@ public class GradebookServiceProxy {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    public void addCourse (CourseDTO course) {
+        sendMessage("addCourse: " + asJsonString(course));
+    }
+
+    public void updateCourse (CourseDTO course) {
+        sendMessage("updateCourse: " + asJsonString(course));
+    }
+
+    public void deleteCourse (String courseId) {
+        sendMessage("deleteCourse: " + courseId);
+    }
+
+    
+
     @RabbitListener(queues = "registrar_service")
     public void receiveFromGradebook(String message)  {
-        //TODO implement this message
+        try {
+            System.out.println("Receive from Gradebook: " + message);
+            String[] parts = message.split(" ", 2);
+            if (parts[0].equals("updateEnrollment")) {
+                EnrollmentDTO dto = fromJsonString(parts[1], EnrollmentDTO.class);
+                Enrollment e = enrollmentRepository.findById(dto.enrollmentId()).orElse(null);
+                if (e == null) {
+                    System.out.println("Error receiveFromGradebook Enrollment not found: " + dto.enrollmentId());
+                } else {
+                    e.setGrade(dto.grade());
+                    enrollmentRepository.save(e);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in receiveFromGradebook: " + e.getMessage());
+        }
     }
 
     private void sendMessage(String s) {
         rabbitTemplate.convertAndSend(gradebookServiceQueue.getName(), s);
     }
+
     private static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
