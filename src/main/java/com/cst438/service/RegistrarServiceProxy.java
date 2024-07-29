436dfc2f754aa665de2c7f657e7306d837dd326b
Chris Loi <loichris97@gmail.com>
2024-07-29 16:40:54 -0700

    updated ReceiveFromRegistrar

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -1,7 +1,6 @@
 package com.cst438.service;
 
-import com.cst438.domain.Enrollment;
-import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.*;
 import com.cst438.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
@@ -18,6 +17,15 @@ public class RegistrarServiceProxy {
 
     @Autowired
     private EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Bean
     public Queue createQueue() {
@@ -56,17 +64,93 @@ public class RegistrarServiceProxy {
     @RabbitListener(queues = "gradebook_service")
     public void receiveFromRegistrar(String message) {
         try {
-            System.out.println("Receive from Registrar: " + message);
+            System.out.println("Receive from Gradebook: " + message);
             String[] parts = message.split(" ", 2);
-            if (parts[0].equals("updateEnrollment")) {
-                EnrollmentDTO dto = fromJsonString(parts[1], EnrollmentDTO.class);
-                Enrollment e = enrollmentRepository.findById(dto.enrollmentId()).orElse(null);
-                if (e == null) {
-                    System.out.println("Error receiveFromRegistrar Enrollment not found: " + dto.enrollmentId());
-                } else {
-                    e.setGrade(dto.grade());
-                    enrollmentRepository.save(e);
-                }
+            String action = parts[0];
+
+            switch (action) {
+                case "addCourse":
+                    CourseDTO courseDTO = fromJsonString(parts[1], CourseDTO.class);
+                    Course course = new Course();
+                    course.setCourseId(courseDTO.courseId());
+                    course.setTitle(courseDTO.title());
+                    course.setCredits(courseDTO.credits());
+                    courseRepository.save(course);
+                    break;
+                case "updateCourse":
+                    CourseDTO courseUpdateDTO = fromJsonString(parts[1], CourseDTO.class);
+                    Course courseUpdate = courseRepository.findById(courseUpdateDTO.courseId()).orElse(null);
+                    if (courseUpdate != null) {
+                        courseUpdate.setTitle(courseUpdateDTO.title());
+                        courseUpdate.setCredits(courseUpdateDTO.credits());
+                        courseRepository.save(courseUpdate);
+                    }
+                    break;
+                case "deleteCourse":
+                    courseRepository.deleteById(parts[1]);
+                    break;
+                case "addSection":
+                    SectionDTO sectionDTO = fromJsonString(parts[1], SectionDTO.class);
+                    Section section = new Section();
+                    section.setSectionNo(sectionDTO.secNo());
+                    section.setSecId(sectionDTO.secId());
+                    section.setBuilding(sectionDTO.building());
+                    section.setRoom(sectionDTO.room());
+                    section.setTimes(sectionDTO.times());
+                    section.setInstructor_email(sectionDTO.instructorEmail());
+                    section.setCourse(courseRepository.findById(sectionDTO.courseId()).orElse(null));
+                    sectionRepository.save(section);
+                    break;
+                case "updateSection":
+                    SectionDTO sectionUpdateDTO = fromJsonString(parts[1], SectionDTO.class);
+                    Section sectionUpdate = sectionRepository.findById(sectionUpdateDTO.secNo()).orElse(null);
+                    if (sectionUpdate != null) {
+                        sectionUpdate.setSecId(sectionUpdateDTO.secId());
+                        sectionUpdate.setBuilding(sectionUpdateDTO.building());
+                        sectionUpdate.setRoom(sectionUpdateDTO.room());
+                        sectionUpdate.setTimes(sectionUpdateDTO.times());
+                        sectionUpdate.setInstructor_email(sectionUpdateDTO.instructorEmail());
+                        sectionUpdate.setCourse(courseRepository.findById(sectionUpdateDTO.courseId()).orElse(null));
+                        sectionRepository.save(sectionUpdate);
+                    }
+                    break;
+                case "deleteSection":
+                    sectionRepository.deleteById(Integer.parseInt(parts[1]));
+                    break;
+                case "addUser":
+                    UserDTO userDTO = fromJsonString(parts[1], UserDTO.class);
+                    User user = new User();
+                    user.setId(userDTO.id());
+                    user.setName(userDTO.name());
+                    user.setEmail(userDTO.email());
+                    userRepository.save(user);
+                    break;
+                case "updateUser":
+                    UserDTO userUpdateDTO = fromJsonString(parts[1], UserDTO.class);
+                    User userUpdate = userRepository.findById(userUpdateDTO.id()).orElse(null);
+                    if (userUpdate != null) {
+                        userUpdate.setName(userUpdateDTO.name());
+                        userUpdate.setEmail(userUpdateDTO.email());
+                        userRepository.save(userUpdate);
+                    }
+                    break;
+                case "deleteUser":
+                    userRepository.deleteById(Integer.parseInt(parts[1]));
+                    break;
+                case "enrollInCourse":
+                    EnrollmentDTO enrollmentDTO = fromJsonString(parts[1], EnrollmentDTO.class);
+                    Enrollment enrollment = new Enrollment();
+                    enrollment.setEnrollmentId(enrollmentDTO.enrollmentId());
+                    enrollment.setSection(sectionRepository.findById(enrollmentDTO.sectionNo()).orElse(null));
+                    enrollment.setStudent(userRepository.findById(enrollmentDTO.studentId()).orElse(null));
+                    enrollment.setGrade(enrollmentDTO.grade());
+                    enrollmentRepository.save(enrollment);
+                    break;
+                case "dropCourse":
+                    enrollmentRepository.deleteById(Integer.parseInt(parts[1]));
+                    break;
+                default:
+                    System.out.println("Unknown action: " + action);
             }
         } catch (Exception e) {
             System.out.println("Exception in receiveFromRegistrar: " + e.getMessage());
